e08bbf6dbc335b2803480686df439382c48014cf
Stephen McLean <stephanmclean93@gmail.com>
2023-11-23 10:09:43 +0000

    add jsdoc comments for lightweight type safety

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ const METER_ID = process.env.METER_ID;
 const OPENVOLT_API_BASE_URL = process.env.OPENVOLT_API_BASE_URL;
 const CARBON_INTENSITY_API_BASE_URL = process.env.CARBON_INTENSITY_API_BASE_URL;
 
+/**
+ * @typedef {Object} EnergyInterval - a single piece of interval data from the OpenVolt API
+ * @property {string} start_interval - the start date of the interval
+ * @property {string} consumption - amount of energy consumed in this interval
+ */
+
+/**
+ * @typedef {Object} CarbonIntensity - carbon intensity measurement
+ * @property {number} actual - actual amount of co2 emitted
+ */
+
+/**
+ * @typedef {Object} CarbonIntensityInterval - a carbon intensity interval from the national grid API
+ * @property {string} from - the start date of the interval
+ * @property {string} to - the end date of the interval
+ * @property {CarbonIntensity} intensity - the intensity measurement for the interval
+ */
+
+/**
+ * Load energy intervals from OpenVolt
+ *
+ * @param {Date} startDate
+ * @param {Date} endDate
+ * @param {string} meterId
+ * @param {string} granularity
+ * @returns {Promise<EnergyInterval[]>}
+ */
 const loadIntervals = async (
   startDate = START_DATE,
   endDate = END_DATE,
@@ -32,37 +59,33 @@ const loadIntervals = async (
   const response = await fetch(url, { headers });
   const json = await response.json();
 
-  return json;
+  return json.data;
 };
 
 /**
- *
- * @param {*} dates ['2023-01-01T00:00:00.000Z', '2023-01-01T00:30:00.000Z']
+ * Load co2 emissions data from national grid API
+ * @param {Date} startDate
+ * @param {Date} endDate
+ * @returns {Promise<CarbonIntensityInterval[]>}
  */
-const loadCO2Emissions = async (dates) => {
-  const promises = dates.map((d) => {
-    const from = new Date(d);
-    from.setMinutes(from.getMinutes() + 30);
+const loadCO2Emissions = async (startDate = START_DATE, endDate = END_DATE) => {
+  const from = new Date(startDate.getTime());
+  from.setMinutes(from.getMinutes() + 30);
 
-    return fetch(
-      `${CARBON_INTENSITY_API_BASE_URL}/intensity/${from.toISOString()}`
-    );
-  });
+  const to = new Date(endDate.getTime());
+  to.setMinutes(to.getMinutes() + 30);
 
-  return Promise.all(promises).then((results) =>
-    Promise.all(results.map((r) => r.json()))
-  );
+  const url = `${CARBON_INTENSITY_API_BASE_URL}/intensity/${from.toISOString()}/${to.toISOString()}`;
+
+  const response = await fetch(url);
+  const json = await response.json();
+
+  return json.data;
 };
 
 const summarizeEnergyAndEmissionData = async () => {
-  const energyIntervalData = await loadIntervals();
-  const energyIntervals = energyIntervalData.data;
-
-  const dateIntervals = energyIntervals.map((i) => i.start_interval);
-
-  const co2Data = (await loadCO2Emissions(dateIntervals)).flatMap(
-    (d) => d.data
-  );
+  const energyIntervals = await loadIntervals();
+  const co2Data = await loadCO2Emissions();
 
   const kwhConsumed = energyIntervals.reduce(
     (acc, val) => acc + Number(val.consumption),
